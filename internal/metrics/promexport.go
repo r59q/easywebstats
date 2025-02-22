524d4f973d92526ca2931507fb89e1db413394c3
A. Malthe Henriksen <mail@r59q.com>
2025-02-22 18:46:55 +0100

    Add exponential rate exports

--- internal/metrics/promexport.go
+++ internal/metrics/promexport.go
@@ -13,20 +13,30 @@ func GetPrometheusExport() string {
 
 	allStats := []string{}
 	allMeans := []string{}
+	allExponentialRates := []string{}
 
 	for _, name := range names {
-		mean, stats := getNumericStat(name)
+		mean, stats, exponentialRates := getNumericStat(name)
 		allMeans = append(allMeans, mean)
 		allStats = append(allStats, strings.Join(stats, "\n"))
+		allExponentialRates = append(allExponentialRates, strings.Join(exponentialRates, "\n"))
 	}
 
 	statsExport := strings.Join(allStats, "\n")
 	meansExport := strings.Join(allMeans, "\n")
-	return fmt.Sprintf("%s\n\n%s", statsExport, meansExport)
+	exponentialRatesExport := strings.Join(allExponentialRates, "\n")
+	return fmt.Sprintf("%s\n\n%s\n\n%s", statsExport, meansExport, exponentialRatesExport)
 }
 
-func getNumericStat(name string) (string, []string) {
-	return getNumericStatMeanExport(name), getNumericStatValues(name)
+func getNumericStat(name string) (string, []string, []string) {
+	return getNumericStatMeanExport(name), getNumericStatValues(name), getNumericExponentialRates(name)
+}
+
+func getNumericStatMeanExport(name string) string {
+	numericStore := datastore.GetNumberStore()
+	mean := numericStore.GetMean(name)
+	fMean := strconv.FormatFloat(mean, 'f', -1, 64)
+	return "ews_stat_mean{name=\"" + name + "\"} " + fMean
 }
 
 func getNumericStatValues(name string) []string {
@@ -43,9 +53,15 @@ func getNumericStatValues(name string) []string {
 	return exports
 }
 
-func getNumericStatMeanExport(name string) string {
+func getNumericExponentialRates(name string) []string {
 	numericStore := datastore.GetNumberStore()
-	mean := numericStore.GetMean(name)
-	fMean := strconv.FormatFloat(mean, 'f', -1, 64)
-	return "ews_stat_mean{name=\"" + name + "\"} " + fMean
+	estimates := numericStore.GetRateEstimates(name)
+	exports := []string{}
+
+	for key, value := range estimates {
+		fVal := strconv.FormatFloat(value, 'f', -1, 64)
+		export := "ews_stat_rate{name=\"" + name + "\",label=\"" + key + "\"} " + fVal
+		exports = append(exports, export)
+	}
+	return exports
 }
